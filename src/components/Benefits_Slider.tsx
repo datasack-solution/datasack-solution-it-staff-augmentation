5fa0e12232a740eafc96d9424b8948a21e8b1c27
karthikraja <pmnkarthikraja@gmail.com>
2024-07-24 17:27:21 +0530

    disable autoplay on benefits slider

--- src/components/Benefits_Slider.tsx
+++ src/components/Benefits_Slider.tsx
@@ -17,7 +17,7 @@ const settings: Settings = {
     speed: 1000,
     slidesToShow: 1,
     slidesToScroll: 1,
-    autoplay: true,
+    autoplay: false,
     autoplaySpeed: 3000,
     cssEase: "linear",
 };
