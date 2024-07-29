27ce94800cb2414f1fc6b6edf28278db1a70ab0e
karthikraja <pmnkarthikraja@gmail.com>
2024-07-29 12:02:05 +0530

    wrap google tag manager in _app.tsx

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,9 +2,11 @@ import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 import '@elastic/eui/dist/eui_theme_light.css';
 import { EuiProvider } from '@elastic/eui';
+import { GoogleTagManager } from '@next/third-parties/google'
 
 export default function App({ Component, pageProps }: AppProps) {
   return <EuiProvider colorMode="light">
     <Component {...pageProps} />
+    <GoogleTagManager gtmId={'AW-16583886159'} />
   </EuiProvider>;
 }
--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -6,6 +6,11 @@ export default function Document() {
       <body>
         <Main />
         <NextScript />
+        <noscript
+    dangerouslySetInnerHTML={{
+      __html: `<iframe src="https://www.googletagmanager.com/ns.html?id=AW-16583886159" height="0" width="0" style="display: none; visibility: hidden;" />`,
+    }}
+  />
       </body>
     </Html>
   );
