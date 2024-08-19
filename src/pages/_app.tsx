0f9ee1e97de829828973a61d60feb1311f57dc19
karthikraja <pmnkarthikraja@gmail.com>
2024-08-19 11:08:36 +0530

    add google analytics tag with body script

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -7,29 +7,30 @@ import { useEffect } from "react";
 import { useRouter } from "next/router";
 
 export default function App({ Component, pageProps }: AppProps) {
-  const router = useRouter();
+  // const router = useRouter();
 
-  useEffect(() => {
-    const handleRouteChange = (url:string) => {
-      window.gtag('config', 'G-RD5F5DH96G', {
-        page_path: url,
-      });
-    };
+  // useEffect(() => {
+  //   const handleRouteChange = (url:string) => {
+  //     window.gtag('config', 'G-RD5F5DH96G', {
+  //       page_path: url,
+  //     });
+  //   };
 
-    router.events.on('routeChangeComplete', handleRouteChange);
+  //   router.events.on('routeChangeComplete', handleRouteChange);
 
-    return () => {
-      router.events.off('routeChangeComplete', handleRouteChange);
-    };
-  }, [router.events]);
+  //   return () => {
+  //     router.events.off('routeChangeComplete', handleRouteChange);
+  //   };
+  // }, [router.events]);
 
   return <EuiProvider colorMode="light">
-     <GoogleAnalytics 
+     {/* <GoogleAnalytics 
         gaId="G-RD5F5DH96G"          
         dataLayerName="it-staff-augmentation-datalayer"   
         key="ga-analytics"
-      />
+      /> */}
     <Component {...pageProps} />
-    <GoogleTagManager gtmId={'AW-16583886159'} />
+    <GoogleTagManager gtmId={'AW-16583886159'}/>
+    <GoogleTagManager gtmId={'GTM-PJGGKTVQ'}/>
   </EuiProvider>;
 }
