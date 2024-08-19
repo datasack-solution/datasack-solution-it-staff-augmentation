62fcafbd8c68c90f39f59ba55d75e264a1ab7fee
karthikraja <pmnkarthikraja@gmail.com>
2024-08-19 11:33:45 +0530

    change next/script to script for getting reflected in header section

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -4,23 +4,24 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head>
-        <Script id="gtag" async src="https://www.googletagmanager.com/gtag/js?id=G-RD5F5DH96G"></Script>
-        <Script id="gtag-config" dangerouslySetInnerHTML={{
+        {/* when using next/script, it is not reflected in header section. */}
+        <script async src="https://www.googletagmanager.com/gtag/js?id=G-RD5F5DH96G"></script>
+        <script dangerouslySetInnerHTML={{
           __html: `window.dataLayer = window.dataLayer || [];
           function gtag(){dataLayer.push(arguments);}
           gtag('js', new Date());
 
           gtag('config', 'G-RD5F5DH96G');`
         }}>
-        </Script>
+        </script>
 
-        <Script id="google-analytics-datalayer" dangerouslySetInnerHTML={{
+        <script dangerouslySetInnerHTML={{
           __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
                   new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
                   j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
                   'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
                   })(window,document,'script','dataLayer','GTM-PJGGKTVQ');`
-        }}></Script>
+        }}></script>
       </Head>
       <body>
         <Main />
