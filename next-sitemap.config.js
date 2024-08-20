75966757e4cded97b8e9ae31503280d7ca901418
karthikraja <pmnkarthikraja@gmail.com>
2024-08-20 13:24:47 +0530

    add sitemap.xml and robots.txt with next-sitemap

---
+++ next-sitemap.config.js
@@ -0,0 +1,12 @@
+// next-sitemap.config.js
+module.exports = {
+    siteUrl: 'https://itstaffaugmentation.datasack.in',
+    generateRobotsTxt: true, // (optional) Generate a robots.txt file
+    sitemapSize: 5000, // (optional) Limit the number of URLs per sitemap file
+    changefreq: 'daily', // (optional) Change frequency of sitemap updates
+    priority: 1.0, // (optional) Default priority for all URLs
+    generateIndexSitemap: false, // (optional) Generate a sitemap index file
+    // You can also specify additional paths here if needed
+    exclude: [],
+  };
+  
