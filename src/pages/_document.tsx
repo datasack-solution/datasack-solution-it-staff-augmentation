cfb1be993a0a6f720cef5f5d6250f2d9cb04cbe0
karthikraja <pmnkarthikraja@gmail.com>
2024-09-13 10:30:22 +0530

    comment the main tag manager

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -4,8 +4,7 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head>
-        {/* Google Tag Manager - Head */}
-        <script
+        {/* <script
           dangerouslySetInnerHTML={{
             __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
                       new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
@@ -13,19 +12,18 @@ export default function Document() {
                       'https://www.googletagmanager.com/gtm.js?id=' + i + dl;f.parentNode.insertBefore(j,f);
                       })(window,document,'script','dataLayer','GTM-PJGGKTVQ');`,
           }}
-        ></script>
+        ></script> */}
 
       </Head>
       <body>
-        {/* Google Tag Manager (noscript) - After <body> tag */}
-        <noscript>
+        {/* <noscript>
           <iframe
             src="https://www.googletagmanager.com/ns.html?id=GTM-PJGGKTVQ"
             height="0"
             width="0"
             style={{ display: "none", visibility: "hidden" }}
           ></iframe>
-        </noscript>
+        </noscript> */}
 
         <Main />
         <NextScript />
