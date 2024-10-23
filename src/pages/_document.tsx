70596b5c145f1235739672de298e73efeb897df1
karthikraja <karthikraja@datasack.in>
2024-10-23 13:37:33 +0530

    modified script tag for gtag

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -4,13 +4,21 @@ export default function Document() {
     <Html lang="en">
       <Head>
         {/* when using next/script, it is not reflected in header section. */}
-        <script dangerouslySetInnerHTML={{
+        {/* <script dangerouslySetInnerHTML={{
           __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
                   new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
                   j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
                   'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
                   })(window,document,'script','dataLayer','GTM-PJGGKTVQ');`
-        }}></script>
+        }}></script> */}
+
+                <script dangerouslySetInnerHTML={{
+                  __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+        new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+        j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+        'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+        })(window,document,'script','dataLayer','GTM-N8CGPQFR');`
+                }}></script>
 
         {/* <script async src="https://www.googletagmanager.com/gtag/js?id=AW-16583886159">
         </script>
@@ -33,8 +41,11 @@ export default function Document() {
           }}
         /> */}
 
-      <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-PJGGKTVQ"
-        height="0" width="0" style={{display:'none',visibility:'hidden'}}></iframe></noscript>
+        {/* <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-PJGGKTVQ"
+          height="0" width="0" style={{ display: 'none', visibility: 'hidden' }}></iframe></noscript> */}
+
+        <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-N8CGPQFR"
+          height="0" width="0" style={{ display: 'none', visibility: 'hidden' }}></iframe></noscript>
       </body>
     </Html>
   );
