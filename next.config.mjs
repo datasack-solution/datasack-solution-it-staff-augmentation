27515481fb25ed6161c9cced2e969eacf825ff67
karthikraja <datasackseodev@gmail.com>
2024-12-12 11:17:55 +0530

    added redirection in next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,18 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+
+  // Adding redirects to your configuration
+  async redirects() {
+    return [
+      {
+        source: '/:path*',  // Matches all paths under itstaffaugmentation.datasack.in
+        destination: 'https://datasack.in/itstaffaugmentation/:path*', // Redirect to the new location
+        statusCode:301,
+        permanent: true,     // This makes it a 301 redirect (permanent)
+      },
+    ];
+  },
 };
 
 export default nextConfig;
