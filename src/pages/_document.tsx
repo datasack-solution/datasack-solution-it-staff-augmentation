7ecded4a286bdeff747b9cdd7f32e332f5ba2f83
karthikraja <datasackseodev@gmail.com>
2024-12-12 15:20:50 +0530

    update the google tag manager id

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -16,12 +16,21 @@ export default function Document() {
 
         {/* use below */}
 
-                <script dangerouslySetInnerHTML={{
+{/* latest comment */}
+                {/* <script dangerouslySetInnerHTML={{
                   __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
         new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
         j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
         'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
         })(window,document,'script','dataLayer','GTM-N8CGPQFR');`
+                }}></script> */}
+
+                <script dangerouslySetInnerHTML={{
+                  __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+        new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+        j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+        'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+        })(window,document,'script','dataLayer','G-RD5F5DH96G');`
                 }}></script>
 
         {/* <script async src="https://www.googletagmanager.com/gtag/js?id=AW-16583886159">
@@ -50,7 +59,11 @@ export default function Document() {
 
           {/* use below */}
 
-        <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-N8CGPQFR"
+{/* latest comment */}
+        {/* <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-N8CGPQFR"
+          height="0" width="0" style={{ display: 'none', visibility: 'hidden' }}></iframe></noscript> */}
+
+           <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=G-RD5F5DH96G"
           height="0" width="0" style={{ display: 'none', visibility: 'hidden' }}></iframe></noscript>
       </body>
     </Html>
