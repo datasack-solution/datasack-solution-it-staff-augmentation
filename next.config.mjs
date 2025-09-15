557bdecf07f72b9c04946dba097dd1874bc06b1d
karthikraja <datasackseodev@gmail.com>
2025-09-15 16:47:06 +0530

    added redirection to datasack.in/itstaffaugmentation

--- next.config.mjs
+++ next.config.mjs
@@ -3,16 +3,16 @@ const nextConfig = {
   reactStrictMode: true,
 
   // Adding redirects to your configuration
-  // async redirects() {
-  //   return [
-  //     {
-  //       source: '/:path*',  // Matches all paths under itstaffaugmentation.datasack.in
-  //       destination: 'https://datasack.in/itstaffaugmentation/:path*', // Redirect to the new location
-  //       statusCode:301,
-  //       permanent: true,     // This makes it a 301 redirect (permanent)
-  //     },
-  //   ];
-  // },
+  async redirects() {
+    return [
+      {
+        source: '/:path*',  // Matches all paths under itstaffaugmentation.datasack.in
+        destination: 'https://datasack.in/itstaffaugmentation/:path*', // Redirect to the new location
+        statusCode:301,
+        permanent: true,     // This makes it a 301 redirect (permanent)
+      },
+    ];
+  },
 };
 
 export default nextConfig;
